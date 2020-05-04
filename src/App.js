a29c5768f3a2a03f0c52f8eadc2b0c1c3e4f3a4a
Rohith Gilla <gillarohith1@gmail.com>
2020-05-04 11:44:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="h-screen bg-gray-900 flex items-center justify-center">
+        <div className="bg-teal-300 rounded-lg p-6 shadow-inner">
+          <span className="text-3xl">React + Tailwind Boilerplate</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
